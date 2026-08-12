01826f416cb548d23e11c71dd5c2dd7ffbe0a503
vefa <44091688+veefa@users.noreply.github.com>
2026-08-12 17:08:46 +0100

    Adding App lyout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,23 @@
-import { StyleSheet, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context"
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [tasks, setTasks] = useState("");
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>Welcome to my app!</Text>
+      <Text style={styles.title}>My Tasks</Text>
+
+      <View style={styles.content}>
+        <TextInput style={styles.input} placeholder="Add a new task..." />
+        <Pressable style={styles.button} onPress={() => console.log("Task added")}>
+          <Text style={styles.buttonText}>Add Task</Text>
+        </Pressable>
+        <Text>Learn React Native</Text>
+        <Text>Build ToDo App</Text>
+        <Text>Exercise</Text>
+      </View>
     </SafeAreaView>
   );
 }
@@ -12,19 +25,33 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
-    justifyContent: "center",
-
-    alignItems: "center",
-
-    backgroundColor: "#cc8f8f",
+    padding: 20,
   },
-
-  text: {
-    color: "#4a1313",
-
+  title: {
+    fontSize: 26,
     fontWeight: "bold",
-
-    fontSize: 24,
+    marginBottom: 24,
+    color: "#570101",
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#570101",
+    padding: 12,
+    borderRadius: 12,
+   
+  },
+  content: {
+    gap: 16,
+  },
+  text: {},
+  button: {
+    backgroundColor: "#570101",
+    padding: 14,
+    borderRadius: 12, 
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
